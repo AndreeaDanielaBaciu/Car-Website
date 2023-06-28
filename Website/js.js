0a1844af0a48d66f0829566083151ef06d8bb77d
AndreeaDanielaBaciu <44543717+AndreeaDanielaBaciu@users.noreply.github.com>
2023-06-28 11:59:27 +0100

    Add files via upload

---
+++ Website/js.js
@@ -0,0 +1,16 @@
+const topDealsScroll = document.querySelector(".top-deals-scroll");
+
+function reverseScroll() {
+  topDealsScroll.style.animation = "scroll 10s infinite linear";
+}
+
+reverseScroll();
+
+const recentScroll = document.querySelector(".recent-scroll");
+
+function reverseScroll() {
+  recentScroll.style.animation = "scroll 10s infinite linear";
+}
+
+reverseScroll();
+
